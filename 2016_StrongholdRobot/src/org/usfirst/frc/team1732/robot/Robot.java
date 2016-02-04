4d96208bd4dd0ca413a718a9382a173c5bc2f999
Jay Schauer <lacrossejason@gmail.com>
2016-02-03 18:53:38 -0600

    committing

--- 2016_StrongholdRobot/src/org/usfirst/frc/team1732/robot/Robot.java
+++ 2016_StrongholdRobot/src/org/usfirst/frc/team1732/robot/Robot.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team1732.robot;
 
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -14,24 +15,22 @@ public class Robot extends IterativeRobot {
 	CANTalon intake1; CANTalon intake2; Solenoid intakeS;
 
 	CANTalon catapult1; CANTalon catapult2; Solenoid catapultS;
-
-	Solenoid shifting;
 	
-	AnalogInput halleffect;
+	double left, right;
+	
+	Joystick controller = new Joystick(0);
 
 	long time;
 	long startTime;
 
 	public void robotInit() {
 		// ports not set
-		left1  = new CANTalon(10); left2  = new CANTalon(11); left3  = new CANTalon(12);
-		right1 = new CANTalon(13); right2 = new CANTalon(14); right3 = new CANTalon(15);
+		left1  = new CANTalon(11); left2  = new CANTalon(15); left3  = new CANTalon(14);
+		right1 = new CANTalon(16); right2 = new CANTalon(17); right3 = new CANTalon(12);
 
-		catapult1 = new CANTalon(16); catapult2 = new CANTalon(17); catapultS = new Solenoid(20);
+		//catapult1 = new CANTalon(16); catapult2 = new CANTalon(17); catapultS = new Solenoid(20);
 
-		intake1 = new CANTalon(18); intake2 = new CANTalon(19); intakeS = new Solenoid(22);
-
-		shifting = new Solenoid(21);
+		//intake1 = new CANTalon(18); intake2 = new CANTalon(19); intakeS = new Solenoid(22);
 		
 	}
 	
@@ -44,16 +43,19 @@ public class Robot extends IterativeRobot {
 	}
 	
     public void teleopPeriodic() {
-    	
-    
+			left = controller.getRawAxis(1);
+			right = controller.getRawAxis(5);
+			left = left * Math.abs(left);
+			right = right * Math.abs(right);
+			setMotors(-left, -right);
     }
     
-	private void setMotor(double left, double right) {
-		left1.set(left * -1);
-		left2.set(left * -1);
-		left3.set(left * -1);
-		right1.set(right);
-		right2.set(right);
+	private void setMotors(double left, double right) {
+		left1.set(left);
+		left2.set(left);
+		left3.set(-left);
+		right1.set(-right);
+		right2.set(-right);
 		right3.set(right);
 	}
 
